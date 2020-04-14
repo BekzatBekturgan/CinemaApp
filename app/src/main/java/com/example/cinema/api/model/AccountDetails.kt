7f1d1811ac6d59f9fdc93aabaf955e098a23b528
BekzatBekturgan <bbekturgan0@gmail.com>
2020-04-14 16:46:09 +0600

    add model account details from server

--- app/src/main/java/com/example/cinema/api/model/AccountDetails.kt
+++ app/src/main/java/com/example/cinema/api/model/AccountDetails.kt
@@ -1,4 +1,11 @@
 package com.example.cinema.api.model
 
-class AccountDetails {
-}
+data class AccountDetails (
+    val hash: String,
+    val id: Int,
+    val iso_639_1: String,
+    val iso_3166_1: String,
+    val name: String,
+    val include_adult: Boolean,
+    val username: String
+)

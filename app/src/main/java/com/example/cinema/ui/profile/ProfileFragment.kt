0ab8ec0f5e0effe4dda5f57c0c552799d339e91e
BekzatBekturgan <bbekturgan0@gmail.com>
2020-04-15 01:56:57 +0600

    add refresh swipe to the fragment

--- app/src/main/java/com/example/cinema/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/cinema/ui/profile/ProfileFragment.kt
@@ -7,12 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.cinema.R
 import com.example.cinema.RetrofitService
 import com.example.cinema.TOKEN_KEY
 import com.example.cinema.api.model.AccountDetails
 import com.example.cinema.api.model.MoviesData
 import com.example.cinema.pref
+import kotlinx.android.synthetic.main.fragment_profiles.*
 import org.w3c.dom.Text
 import retrofit2.Call
 import retrofit2.Callback
@@ -20,6 +22,7 @@ import retrofit2.Response
 
 class ProfileFragment: Fragment() {
     private lateinit var rootView: View
+    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
     private var sessionId: String? = null
     private var textViewName: TextView? = null
     private var textViewUsername: TextView? = null
@@ -36,16 +39,22 @@ class ProfileFragment: Fragment() {
         sessionId = pref.getString("sessionID", "empty")
 
         rootView = inflater.inflate(R.layout.fragment_profiles, container, false)
+        swipeRefreshLayout = rootView.findViewById(R.id.swipeRefreshLayoutProfile)
         getAccountDetails()
+        swipeRefreshLayout.setOnRefreshListener {
+            getAccountDetails()
+        }
         bindViews(rootView)
         return rootView
     }
     private fun bindViews(rootView: View) {
         textViewName = rootView.findViewById(R.id.textViewProfileName)
         textViewUsername = rootView.findViewById(R.id.textViewProfileEmail)
+
     }
 
     private fun getAccountDetails() {
+        swipeRefreshLayout.isRefreshing = true
         RetrofitService.getMovieApi().getAccountDetails(sessionId)
             ?.enqueue(object : Callback<AccountDetails?> {
                 override fun onFailure(call: Call<AccountDetails?>, t: Throwable) {
@@ -69,6 +78,7 @@ class ProfileFragment: Fragment() {
                     else{
                         Log.d("Response fail", "response failed")
                     }
+                    swipeRefreshLayout.isRefreshing = false
                 }
             })
     }

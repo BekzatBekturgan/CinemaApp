0167fecc3fbf9f956d4f96f7741f0ff0dc1dd256
BekzatBekturgan <bbekturgan0@gmail.com>
2020-04-14 17:25:57 +0600

    Merge branch 'feature/Bagdan' of https://github.com/MagiaGroz/CinemaApp into feature/Bekzat
     Conflicts:
    	app/src/main/java/com/example/cinema/RetrofitService.kt
    	app/src/main/java/com/example/cinema/api/service/UserClient.kt

--- app/src/main/java/com/example/cinema/api/service/UserClient.kt
+++ app/src/main/java/com/example/cinema/api/service/UserClient.kt
@@ -13,12 +13,43 @@ interface UserClient {
     fun getSessionId(@Body requestSessionId : RequestSessionId): Call<SessionId>
 
 
-    /*@GET("authentication/token/new?api_key=753b84576c954d96997803298a188f83")
+    @GET("authentication/token/new?api_key=753b84576c954d96997803298a188f83")
     fun getToken():Call<Token>
 
-     */
     @GET("posts")
     fun getPostList(): Call<List<Post>>
     @GET("posts/{id}")
     fun getPostById(@Path("id") id: Int): Call<Post>
+    @GET("movie/popular")
+    fun getPopularMovies(@Query("api_key") apiKey: String = "753b84576c954d96997803298a188f83",
+                         @Query("page") page: Int): Call<MovieResponse>
+    @GET("movie/{movie_id}")
+    fun getMovieById(@Path("movie_id") movieId: Int=1,
+                     @Query("api_key") apiKey: String = "753b84576c954d96997803298a188f83"):Call<MoviesData>
+
+    @GET("account/{account_id}/favorite/movies?api_key=753b84576c954d96997803298a188f83")
+    fun getFavouriteMovies(
+        //@Path("account_id") id: Int,
+        @Query("session_id") sessionId: String?
+    ): Call<MovieResponse>
+
+    @Headers("Content-Type:application/json; charset=UTF-8")
+    @POST("account/{account_id}/favorite?api_key=753b84576c954d96997803298a188f83")
+    fun addFavList(
+        @Body movie: FavMovieInfo,
+        @Query("session_id") session: String?
+    ): Call<FavResponse>
+
+    @GET("movie/{movie_id}/account_states")
+    fun getMovieState(
+        @Path("movie_id") id: Int,
+        @Query("api_key") apiKey: String?,
+        @Query("session_id") session: String?
+    ): Call<MoviesData?>?
+
+    @GET("account?api_key=753b84576c954d96997803298a188f83")
+    fun getAccountDetails(
+        @Query("session_id") session: String?
+    ) : Call<AccountDetails>
+
 }

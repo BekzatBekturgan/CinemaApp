c1dc8cc9317362947f5fcedceb4cbd9eb75e96f1
BekzatBekturgan <bbekturgan0@gmail.com>
2020-04-14 11:03:56 +0600

    creating process get session id

--- app/src/main/java/com/example/cinema/AuthActivity.kt
+++ app/src/main/java/com/example/cinema/AuthActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.cinema.api.model.Login
+import com.example.cinema.api.model.SessionId
 import com.example.cinema.api.model.Token
 import kotlinx.android.synthetic.main.activity_auth.*
 import retrofit2.Call
@@ -20,69 +21,105 @@ class AuthActivity : AppCompatActivity() {
 
         pref = getSharedPreferences(APP_PREFERENCES, MODE_PRIVATE)
         getToken()
-        login_button.setOnClickListener{
+        login_button.setOnClickListener {
             login()
         }
 
 
-
     }
-    fun login(){
-        var token = pref.getString(TOKEN_KEY,"error")
-        if(token!="error") {
-            var login: Login = Login(login_view.text.toString(), password_view.text.toString(),token)
+
+    fun login() {
+        var token = pref.getString(TOKEN_KEY, "error")
+        if (token != "error") {
+            var login: Login =
+                Login(login_view.text.toString(), password_view.text.toString(), token)
             var call: Call<Token> = RetrofitService.getPostApi().login(login)
             call.enqueue(object : Callback<Token> {
                 override fun onFailure(call: Call<Token>, t: Throwable) {
-                    Toast.makeText(this@AuthActivity, "ERROR WITH CONNECTION", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this@AuthActivity, "ERROR WITH CONNECTION", Toast.LENGTH_SHORT)
+                        .show()
                 }
 
                 override fun onResponse(call: Call<Token>, response: Response<Token>) {
-                    if(response.isSuccessful){
-                        var answer:Token? = response.body()
-                        if(answer!!.success==true){
+                    if (response.isSuccessful) {
+                        var answer: Token? = response.body()
+                        if (answer!!.success == true) {
                             val intent = Intent(this@AuthActivity, MainActivity::class.java)
                             //        val bundle = Bundle()
                             //        bundle.putSerializable("item_el", item)
                             //        intent.putExtras(bundle)
+                            getSessionId()
                             startActivity(intent)
-                        }
-                        else{
+                        } else {
 
                         }
-                    }
-                    else{
-                        Toast.makeText(this@AuthActivity, "Login or password is not correct", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(
+                            this@AuthActivity,
+                            "Login or password is not correct",
+                            Toast.LENGTH_SHORT
+                        ).show()
 
                     }
                 }
             })
-        }
-        else{
-            Log.d("Shared preferences","Could not correctly get token from shared preferences")
+        } else {
+            Log.d("Shared preferences", "Could not correctly get token from shared preferences")
         }
     }
-    fun getToken(){  //получить токен с сервера
 
-        var call : Call<Token> =  RetrofitService.getPostApi().getToken()
-        call.enqueue(object : Callback<Token>{
+    fun getToken() {  //получить токен с сервера
+
+        var call: Call<Token> = RetrofitService.getPostApi().getToken()
+        call.enqueue(object : Callback<Token> {
             override fun onFailure(call: Call<Token>, t: Throwable) {
                 Toast.makeText(this@AuthActivity, "ERROR", Toast.LENGTH_SHORT).show()
             }
 
             override fun onResponse(call: Call<Token>, response: Response<Token>) {
-                if(response.isSuccessful){
-                    var answer:Token? = response.body()
+                if (response.isSuccessful) {
+                    var answer: Token? = response.body()
                     val editor = pref.edit()
-                    editor.putString(TOKEN_KEY, answer!!.request_token)        // кладем полученный токен в shared preferences
+                    editor.putString(
+                        TOKEN_KEY,
+                        answer!!.request_token
+                    )        // кладем полученный токен в shared preferences
                     editor.apply()
-                }
-                else{
-                    Toast.makeText(this@AuthActivity, "Api key is not correct ", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this@AuthActivity, "Api key is not correct ", Toast.LENGTH_SHORT)
+                        .show()
 
                 }
             }
         })
+    }
+
+    fun getSessionId() {
+        var token = pref.getString(TOKEN_KEY, "error")
+        if (token != "error") {
+            var call: Call<SessionId> = RetrofitService.getPostApi().getSessionId()
+            call.enqueue(object : Callback<SessionId> {
+                override fun onFailure(call: Call<SessionId>, t: Throwable) {
+                    Toast.makeText(this@AuthActivity, "ERROR WITH CONNECTION", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                override fun onResponse(call: Call<SessionId>, response: Response<SessionId>) {
+                    if (response.body()?.success == true) {
+                        Log.d("pusk", response.body()?.session_id)
+                        val edt = pref.edit()
+                        edt.putString("sessionID", response.body()?.session_id)
+                        edt.apply()
+                    } else {
+                        Toast.makeText(
+                            this@AuthActivity,
+                            "Login or password is not correct",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            })
+        }
     }
 }
 

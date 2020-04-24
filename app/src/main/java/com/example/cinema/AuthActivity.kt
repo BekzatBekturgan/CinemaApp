01a0435cf7c993b55856d8c2f8f8493d730c08b6
MagiaGroz <grozaga@gmail.com>
2020-04-25 05:24:27 +0600

    Token, login coroutines

--- app/src/main/java/com/example/cinema/AuthActivity.kt
+++ app/src/main/java/com/example/cinema/AuthActivity.kt
@@ -10,11 +10,19 @@ import com.example.cinema.api.model.RequestSessionId
 import com.example.cinema.api.model.SessionId
 import com.example.cinema.api.model.Token
 import kotlinx.android.synthetic.main.activity_auth.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import kotlin.coroutines.CoroutineContext
 
-class AuthActivity : AppCompatActivity() {
+class AuthActivity : AppCompatActivity(), CoroutineScope {
+    private val job = Job()
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,72 +37,55 @@ class AuthActivity : AppCompatActivity() {
 
     }
 
-    fun login() {
-        var token = pref.getString(TOKEN_KEY, "error")
-        if (token != "error") {
-            var login: Login =
-                Login(login_view.text.toString(), password_view.text.toString(), token)
-            var call: Call<Token> = RetrofitService.getMovieApi().login(login)
-            call.enqueue(object : Callback<Token> {
-                override fun onFailure(call: Call<Token>, t: Throwable) {
-                    Toast.makeText(this@AuthActivity, "ERROR WITH CONNECTION", Toast.LENGTH_SHORT)
-                        .show()
-                }
-
-                override fun onResponse(call: Call<Token>, response: Response<Token>) {
-                    if (response.isSuccessful) {
-                        var answer: Token? = response.body()
-                        if (answer?.success == true) {
-                            val intent = Intent(this@AuthActivity, MainActivity::class.java)
-                            //        val bundle = Bundle()
-                            //        bundle.putSerializable("item_el", item)
-                            //        intent.putExtras(bundle)
-                            Log.d("token response", "success")
-                            getSessionId()
-                            startActivity(intent)
-                        } else {
-
-                        }
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
+    }
+    private fun login() {
+        launch {
+            var token = pref.getString(TOKEN_KEY, "error")
+            if (token != "error") {
+                var login: Login =
+                    Login(login_view.text.toString(), password_view.text.toString(), token)
+                val response = RetrofitService.getMovieApi().login(login)
+                if (response.isSuccessful) {
+                    var answer: Token? = response.body()
+                    if (answer?.success == true) {
+                        val intent = Intent(this@AuthActivity, MainActivity::class.java)
+                        Log.d("token response", "success")
+                        getSessionId()
+                        startActivity(intent)
                     } else {
                         Toast.makeText(
                             this@AuthActivity,
-                            "Login or password is not correct",
+                            "ERROR WITH CONNECTION",
                             Toast.LENGTH_SHORT
-                        ).show()
-
+                        )
+                            .show()
                     }
                 }
-            })
-        } else {
-            Log.d("Shared preferences", "Could not correctly get token from shared preferences")
+            }
         }
     }
 
-    fun getToken() {  //получить токен с сервера
-
-        var call: Call<Token> = RetrofitService.getMovieApi().getToken()
-        call.enqueue(object : Callback<Token> {
-            override fun onFailure(call: Call<Token>, t: Throwable) {
-                Toast.makeText(this@AuthActivity, "ERROR", Toast.LENGTH_SHORT).show()
+    fun getToken() {  //get token from server
+        launch {
+            val response = RetrofitService.getMovieApi().getToken()
+            if (response.isSuccessful) {
+                var answer: Token? = response.body()
+                val editor = pref.edit()
+                editor.putString(
+                    TOKEN_KEY,
+                    answer?.request_token
+                )        // кладем полученный токен в shared preferences
+                editor.apply()
+                Log.d("get token", pref.getString(TOKEN_KEY, answer?.request_token))
+            } else {
+                Toast.makeText(this@AuthActivity, "Api key is not correct ", Toast.LENGTH_SHORT)
+                    .show()
             }
+        }
 
-            override fun onResponse(call: Call<Token>, response: Response<Token>) {
-                if (response.isSuccessful) {
-                    var answer: Token? = response.body()
-                    val editor = pref.edit()
-                    editor.putString(
-                        TOKEN_KEY,
-                        answer?.request_token
-                    )        // кладем полученный токен в shared preferences
-                    editor.apply()
-                    Log.d("get token", pref.getString(TOKEN_KEY, answer?.request_token))
-                } else {
-                    Toast.makeText(this@AuthActivity, "Api key is not correct ", Toast.LENGTH_SHORT)
-                        .show()
-
-                }
-            }
-        })
     }
 
     fun getSessionId() {
@@ -129,5 +120,7 @@ class AuthActivity : AppCompatActivity() {
             })
         }
     }
+
+
 }
 
--- app/src/main/java/com/example/cinema/api/service/UserClient.kt
+++ app/src/main/java/com/example/cinema/api/service/UserClient.kt
@@ -2,18 +2,19 @@ package com.example.cinema.api.service
 
 import com.example.cinema.api.model.*
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.*
 
 const val apiKey = "753b84576c954d96997803298a188f83"
 interface UserClient {
     @POST("authentication/token/validate_with_login?api_key=753b84576c954d96997803298a188f83")
-    fun login(@Body login:Login): Call<Token>
+    suspend fun login(@Body login:Login): Response<Token>
     // create a new session_id
     @POST("authentication/session/new?api_key=753b84576c954d96997803298a188f83")
     fun getSessionId(@Body requestSessionId : RequestSessionId): Call<SessionId>
 
     @GET("authentication/token/new?api_key=753b84576c954d96997803298a188f83")
-    fun getToken():Call<Token>
+    suspend fun getToken(): Response<Token>
 
     @GET("posts")
     fun getPostList(): Call<List<Post>>

cfceeeb088804c74f3f21acdd020effd5be38843
BekzatBekturgan <bbekturgan0@gmail.com>
2020-04-25 02:42:24 +0600

    delete some errors

--- app/src/main/java/com/example/cinema/api/model/Post.kt
+++ app/src/main/java/com/example/cinema/api/model/Post.kt
@@ -4,9 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
-@Entity(tableName = "post_table")
 data class Post(
-    @PrimaryKey
     @SerializedName("id") val postId: Int,
     @SerializedName("userId") val userId: Int,
     @SerializedName("title") val title: String,

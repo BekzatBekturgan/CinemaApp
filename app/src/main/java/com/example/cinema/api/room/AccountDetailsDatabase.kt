c2067295a11419dc4946de8106e2e2d7e1712cbd
BekzatBekturgan <bbekturgan0@gmail.com>
2020-04-25 22:47:00 +0600

    adding interface profile dao

--- app/src/main/java/com/example/cinema/api/room/AccountDetailsDatabase.kt
+++ app/src/main/java/com/example/cinema/api/room/AccountDetailsDatabase.kt
@@ -1,10 +1,29 @@
 package com.example.cinema.api.room
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.cinema.api.model.AccountDetails
 
 @Database(entities = [AccountDetails::class], version = 1)
 abstract class AccountDetailsDatabase : RoomDatabase(){
+
+
+    companion object {
+
+        var INSTANCE: AccountDetailsDatabase? = null
+
+        fun getDatabase(context: Context): AccountDetailsDatabase {
+            if (INSTANCE == null) {
+                INSTANCE = Room.databaseBuilder(
+                    context.applicationContext,
+                    AccountDetailsDatabase::class.java,
+                    "account_details_database.db"
+                ).build()
+            }
+            return INSTANCE!!
+        }
+    }
 
 }

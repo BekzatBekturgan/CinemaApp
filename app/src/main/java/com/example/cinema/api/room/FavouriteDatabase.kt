84ca79447a6f92d9a02a499dd0356386ee82aa1a
BekzatBekturgan <bbekturgan0@gmail.com>
2020-04-27 03:42:53 +0600

    little changes

--- app/src/main/java/com/example/cinema/api/room/FavouriteDatabase.kt
+++ app/src/main/java/com/example/cinema/api/room/FavouriteDatabase.kt
@@ -4,15 +4,18 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.cinema.api.model.FavouriteMovies
 
-@Database(entities = [FavouriteMovies::class], version = 2)
+@Database(entities = [FavouriteMovies::class], version = 1)
 abstract class FavouriteDatabase : RoomDatabase() {
 
     abstract fun favMoviesDao(): FavouriteDao
 
     companion object {
-
+        @JvmField
+        //val MIGRATION_1_2 = Migration1To2()
         var INSTANCE: FavouriteDatabase? = null
 
         fun getDatabase(context: Context): FavouriteDatabase {
@@ -26,4 +29,15 @@ abstract class FavouriteDatabase : RoomDatabase() {
             return INSTANCE!!
         }
     }
+    /*
+    class Migration1To2 : Migration(1, 2) {
+        override fun migrate(database: SupportSQLiteDatabase) {
+            database.execSQL(
+                "ALTER TABLE favourite_movie_table "
+                        + "DROP COLUMN ListData"
+            )
+        }
+    }
+    */
+
 }

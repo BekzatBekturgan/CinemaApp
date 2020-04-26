7acf0749268a657a6a7455518c09f794a7fdea6c
BekzatBekturgan <bbekturgan0@gmail.com>
2020-04-26 07:26:21 +0600

    sql requests

--- app/src/main/java/com/example/cinema/api/room/FavouriteDao.kt
+++ app/src/main/java/com/example/cinema/api/room/FavouriteDao.kt
@@ -1,4 +1,17 @@
 package com.example.cinema.api.room
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.cinema.api.model.FavouriteMovies
+
+@Dao
 interface FavouriteDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertFav(list: List<FavouriteMovies>)
+
+    @Query("Select * from favourite_movie_table ")
+    fun getFav(): List<FavouriteMovies>
 }
